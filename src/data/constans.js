32dd6e79713651d82bfcd940247208fe58a7a858
vat332 <oporowymakus21@gmail.com>
2025-05-21 15:25:10 +0200

    Add languages data structure to define programming languages with styles

---
+++ src/data/constans.js
@@ -0,0 +1,39 @@
+const languages = [
+  {
+    name: "HTML",
+    backgroundColor: "#E2680F",
+    color: "#F9F4DA",
+  },
+  {
+    name: "CSS",
+    backgroundColor: "#328AF1",
+    color: "#F9F4DA",
+  },
+  {
+    name: "JavaScript",
+    backgroundColor: "#f4eb13",
+    color: "#1e1e1e",
+  },
+  {
+    name: "React",
+    backgroundColor: "#2ed3e9",
+    color: "#1e1e1e",
+  },
+  {
+    name: "TypeScript",
+    backgroundColor: "#298ec6",
+    color: "#F9F4DA",
+  },
+  {
+    name: "Node.js",
+    backgroundColor: "#599137",
+    color: "#F9F4DA",
+  },
+  {
+    name: "Python",
+    backgroundColor: "#ffd742",
+    color: "#1e1e1e",
+  },
+];
+
+export default languages;
